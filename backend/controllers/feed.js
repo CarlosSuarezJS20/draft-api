bcad1430c3a9b69ab5f0c9f5ded5fcefe03b06f1
CarlosSuarezJS20 <69536882+CarlosSuarezJS20@users.noreply.github.com>
2024-05-13 15:56:53 +0100

    adding backend

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,15 +1,34 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: "dummy_id_1",
+        title: "First Post",
+        content: "This is the first post!",
+        creator: { name: "Carlos" },
+        imageURL: "images/duck.jpg",
+        createdAt: new Date(),
+      },
+      {
+        _id: "dummy_id_2",
+        title: "Second Post",
+        content: "This is the Second post!",
+        creator: { name: "Carlos" },
+        imageURL: "images/duck.jpg",
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
+  let imageURL;
+
   // Create post in db
   res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    message: "Post created successfully!",
+    post: req.body,
   });
 };
